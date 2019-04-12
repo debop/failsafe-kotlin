4ace2b9abd492c17fea830b623a0b27e6a260d97
debop <debop@coupang.com>
2019-04-12 18:09:36 +0900

    Bump up kotlin-stdlib 1.3.30

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,6 @@
-/*
- * Copyright (c) 2017-2020 by Coupang. Some rights reserved.
- * See the project homepage at: http://gitlab.coupang.net
- * Licensed under the Coupang License;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://coupang.net/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Copyright (c) 2017-2020 by Coupang. Some rights reserved.
- * See the project homepage at: http://gitlab.coupang.net
- * Licensed under the Coupang License;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://coupang.net/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Copyright (c) 2017-2020 by Coupang. Some rights reserved.
- * See the project homepage at: http://gitlab.coupang.net
- * Licensed under the Coupang License;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://coupang.net/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.3.21")
+    id("org.jetbrains.kotlin.jvm").version("1.3.30")
 }
 
 repositories {
