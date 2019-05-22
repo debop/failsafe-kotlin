e08409dd158372f1c67dc595b70977e51796b26c
debop <debop@coupang.com>
2019-05-23 01:39:51 +0900

    Bump up kotlin-stdlib, kotlinx-coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,21 @@
+/*
+ * Copyright (c) 2017-2020 by Coupang. Some rights reserved.
+ * See the project homepage at: http://gitlab.coupang.net
+ * Licensed under the Coupang License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://coupang.net/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.3.30")
+    id("org.jetbrains.kotlin.jvm").version("1.3.31")
 }
 
 repositories {
@@ -9,7 +24,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
 
     compile("net.jodah:failsafe:2.0.1")
     compile("io.github.microutils:kotlin-logging:1.6.23")
