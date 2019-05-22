71f1b3581d72c36313342d1fdb98361739f8ce09
debop <debop@coupang.com>
2019-05-23 01:46:11 +0900

    Bump up gradle wrapper, junit 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright (c) 2017-2020 by Coupang. Some rights reserved.
+ * See the project homepage at: http://gitlab.coupang.net
+ * Licensed under the Coupang License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://coupang.net/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 plugins {
     id("org.jetbrains.kotlin.jvm").version("1.3.31")
 }
@@ -34,8 +50,8 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
 
     testImplementation("org.amshove.kluent:kluent:1.47")
 }
